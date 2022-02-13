aad65197f4a90cdb6dd7422bb4382af787fd1341
rohmanservice1 <rohmanservice1@outlook.com>
2022-02-14 02:35:11 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  tem plateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
